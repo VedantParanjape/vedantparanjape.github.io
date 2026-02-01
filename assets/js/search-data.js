18addcd62ea371d14c1a516f7bed52f26983019f
VedantParanjape <vedantparanjape160201@gmail.com>
2026-02-01 23:35:44 +0000

    Deploying to gh-pages from @ VedantParanjape/vedantparanjape.github.io@be92291b709c6cf822e4b360d894253dc843b27b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -170,7 +170,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=7HuvXGoAAAAJ", "_blank");
         },
       },{
         id: 'social-x',
